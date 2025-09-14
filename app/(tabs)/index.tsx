f48c9703f619454358a69b58dbf6c3bc6bcde891
rushikesh1629 <rushikeshbhor1629@gmail.com>
2025-09-14 11:27:06 +0530

    final commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,26 @@
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { use } from 'react'
-import Typo from '@/components/Typo'
-import Button from '@/components/Button'
-import { colors, spacingX, spacingY } from '@/constants/theme'
-import { signOut } from 'firebase/auth'
-import { auth } from '@/config/firebase'
-import { useAuth } from '@/context/authContext'
-import ScreenWrapper from '@/components/ScreenWrapper'
-import { verticalScale } from '@/utils/styling'
-import * as Icons from 'phosphor-react-native';
-import HomeCard from '@/components/HomeCard'
-import TransactionList from '@/components/TransactionList'
-import { useRouter } from 'expo-router'
-import { limit, orderBy, where } from 'firebase/firestore'
-import useFetchData from '@/hooks/useFetchData'
-import { TransactionType } from '@/types'
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { use } from "react";
+import Typo from "@/components/Typo";
+import Button from "@/components/Button";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { signOut } from "firebase/auth";
+import { auth } from "@/config/firebase";
+import { useAuth } from "@/context/authContext";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import { verticalScale } from "@/utils/styling";
+import * as Icons from "phosphor-react-native";
+import HomeCard from "@/components/HomeCard";
+import TransactionList from "@/components/TransactionList";
+import { useRouter } from "expo-router";
+import { limit, orderBy, where } from "firebase/firestore";
+import useFetchData from "@/hooks/useFetchData";
+import { TransactionType } from "@/types";
 
 const Home = () => {
   const { user } = useAuth();
@@ -24,55 +30,73 @@ const Home = () => {
   const constraints = [
     where("uid", "==", user?.uid),
     orderBy("date", "desc"),
-    limit(30)
+    limit(30),
   ];
 
   const {
     data: recentTransactions,
     error,
-    loading: transactionLoading
-  } = useFetchData<TransactionType>('transactions', constraints);
+    loading: transactionLoading,
+  } = useFetchData<TransactionType>("transactions", constraints);
 
   return (
     <ScreenWrapper>
       <View style={styles.container}>
         {/* Header */}
         <View style={styles.header}>
-
           <View style={{ gap: 4 }}>
             <Typo size={16} color={colors.neutral400}>
               Hello,
             </Typo>
-            <Typo size={20} fontWeight={'500'}>
+            <Typo size={20} fontWeight={"500"}>
               {user?.name}
             </Typo>
           </View>
 
-          <TouchableOpacity style={styles.searchIcon}>
-            <Icons.MagnifyingGlassIcon size={verticalScale(20)} color={colors.neutral200} weight='bold' />
+          <TouchableOpacity
+            onPress={() => router.push("/(modals)/searchModal")}
+            style={styles.searchIcon}
+          >
+            <Icons.MagnifyingGlassIcon
+              size={verticalScale(20)}
+              color={colors.neutral200}
+              weight="bold"
+            />
           </TouchableOpacity>
-
         </View>
 
-        <ScrollView contentContainerStyle={styles.scrollViewStyle} showsVerticalScrollIndicator={false}>
+        <ScrollView
+          contentContainerStyle={styles.scrollViewStyle}
+          showsVerticalScrollIndicator={false}
+        >
           <View>
             <HomeCard />
           </View>
 
-          <TransactionList loading={transactionLoading} emptyListMessage='No Transactions Added Yet!' data={recentTransactions} title='Recent Transctions' />
+          <TransactionList
+            loading={transactionLoading}
+            emptyListMessage="No Transactions Added Yet!"
+            data={recentTransactions}
+            title="Recent Transctions"
+          />
         </ScrollView>
 
-        <Button style={styles.floatingButton} onPress={() => router.push('/(modals)/transactionModal')}>
-          <Icons.PlusIcon color={colors.black} weight='bold' size={verticalScale(24)} />
+        <Button
+          style={styles.floatingButton}
+          onPress={() => router.push("/(modals)/transactionModal")}
+        >
+          <Icons.PlusIcon
+            color={colors.black}
+            weight="bold"
+            size={verticalScale(24)}
+          />
         </Button>
-
       </View>
-
     </ScreenWrapper>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
   container: {
@@ -82,8 +106,8 @@ const styles = StyleSheet.create({
   },
   header: {
     flexDirection: "row",
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: spacingY._10,
   },
   searchIcon: {
@@ -95,7 +119,7 @@ const styles = StyleSheet.create({
     height: verticalScale(50),
     width: verticalScale(50),
     borderRadius: 100,
-    position: 'absolute',
+    position: "absolute",
     bottom: verticalScale(30),
     right: verticalScale(30),
   },
@@ -104,4 +128,4 @@ const styles = StyleSheet.create({
     paddingBottom: verticalScale(100),
     gap: spacingY._25,
   },
-})
+});
